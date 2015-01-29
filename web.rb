7d74d3c148fee5645db80835455fb99720798e68
zunda <zundan@gmail.com>
2015-01-28 16:51:36 -1000

    Add tasty dango

--- web.rb
+++ web.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
 
 get '/' do
-	"Hello from dango"
+	<<_HTML
+<html>
+<head>
+<title>dango</title>
+</head>
+<body style="
+	background-color: #120d0a;
+	color: white;
+	height: 100%;
+	background-image: url(http://photos-h.ak.instagram.com/hphotos-ak-xpa1/t51.2885-15/1941230_1408222796098551_1531280107_n.jpg);
+	background-position: center;
+	background-repeat: no-repeat;">
+	<h1>Hello from Dango</h1>
+</body>
+</html>
+_HTML
 end
 
 get '/quit' do
