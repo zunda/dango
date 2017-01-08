2867e63835f2acaf5571c166d3c0e93fd3243101
zunda <zundan@gmail.com>
2017-01-07 14:22:06 -1000

    Stop worker dyno from stopping

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ class IO
 	end
 end
 
-$stdout.puts_and_flush "Hi, I am stopping"
-$stdout.puts_and_flush "in about 10 seconds"
-sleep 10
-$stdout.puts_and_flush "Bye"
+loop do
+	$stdout.puts_and_flush "Hi, I am repeating this in about 10 seconds"
+	sleep 10
+end
